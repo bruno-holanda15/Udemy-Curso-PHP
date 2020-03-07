617c36042c033c506bddcfb88197af80d3016224
bruno-holanda15 <br.holanda15@gmail.com>
2020-03-07 16:25:34 -0300

    Iniciando curso de PHP alura - lista de array

---
+++ alura/avancado/lista.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$listaIdade = ['22','21','18','17','28','29'];
+
+for ($i=0; $i < count($listaIdade) ; $i++) { 
+  
+    // echo "Item ".$i." - ".$listaIdade[$i]."<br>";
+    // maneira de exibir o resultado no terminal ou Git Bash
+    echo $listaIdade[$i] . PHP_EOL;
+
+
+};
